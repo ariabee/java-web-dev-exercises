360728999bb8ac42d3ec26a58271be86e0869ace
ariabee <abrown72@uvm.edu>
2020-05-08 15:33:16 -0400

    testing interfaces that extend interfaces

---
+++ src/testing/interfaces/Movable.java
@@ -0,0 +1,6 @@
+package testing.interfaces;
+
+public interface Movable {
+
+    public static final String EXAMPLE = "moving";
+}
--- src/testing/interfaces/Walkable.java
+++ src/testing/interfaces/Walkable.java
@@ -1,6 +1,6 @@
 package testing.interfaces;
 
-public interface Walkable {
+public interface Walkable extends Movable {
 
     void walk(double distance);
 }
